2022bbc19491239dfc473293af2d933b8af31cad
Elyessto <66465116+Elyessto@users.noreply.github.com>
2025-05-08 13:44:49 +0100

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -8,18 +8,15 @@ while true; do
   EXISTS=$(mysql discuit  -e  "SELECT id FROM users WHERE username = '${admin_user}';")
   if [ "$EXISTS" !=  "" ]; then
     echo "User '$admin_user'  found in database."
+    echo "Making user ${user_admin} admin"
+    mysql discuit  -e  "UPDATE users SET is_admin = 1 WHERE id = (SELECT id FROM users WHERE username = '${admin_user}' );"
+    echo "Starting App From Loop"
+    /app/discuit serve 
     break
   else 
-     pkill discuit
-     /app/discuit serve & 
      echo "Hello ${admin_user}"
      echo "sleep 20 s"
      sleep 20
   fi
-echo "starting out of the loop"
-echo "$EXISTS"
-echo "Making user ${user_admin} admin"
-mysql discuit  -e  "UPDATE users SET is_admin = 1 WHERE id = (SELECT id FROM users WHERE username = '${admin_user}' );"
-echo "starting APP"
-/app/discuit serve 
+
 done
