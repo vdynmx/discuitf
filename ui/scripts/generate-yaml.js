71f7eed84ebe008bd244ae413a29762dd8f362a2
Elyessto <66465116+Elyessto@users.noreply.github.com>
2025-05-08 10:26:15 +0100

    Update generate-yaml.js

--- ui/scripts/generate-yaml.js
+++ ui/scripts/generate-yaml.js
@@ -1,13 +1,31 @@
 import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Get the root path
-const rootPath = path.join(import.meta.dirname, '../../');
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//const rootPath = '/root/discuit/';
+//const rootPath = path.join(import.meta.dirname, '../../');
+try {
+  console.log('About to compute rootPath...');
+  console.log('__filename:', __filename);
+  console.log('__dirname:', __dirname);
+} catch (err) {
+  console.error('Error computing rootPath:', err);
+}
+const rootPath = path.resolve(__dirname, '../..');
+
+  console.log('Root path:', rootPath);
+  console.log('__filename:', __filename);
+  console.log('__dirname:', __dirname);
+ 
 // Run the command to get the config
+
 (async () => {
-  await exec('./discuit inject-config', { cwd: '../' }, async (error, stdout, stderr) => {
+  await exec('./discuit inject-config', { cwd: rootPath }, async (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
