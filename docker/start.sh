da07b7eebbbe463c1df5b379283ddd7741f5ec53
Elyessto <66465116+Elyessto@users.noreply.github.com>
2025-05-08 12:27:13 +0100

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -6,13 +6,14 @@ echo "Waiting for user '${admin_user}' to appear in database..."
 
 while true; do
   EXISTS=$(mysql discuit  -e  "SELECT * FROM users WHERE username = '${admin_user}';")
-  if [ $? ==  0 ]; then
+  if [ "$?" ==  0 ]; then
     echo "User '$admin_user'  found in database."
     break
   else 
      pkill discuit
      /app/discuit serve & 
      echo "Hello ${admin_user}"
+     echo "sleep 20 s"
      sleep 20
   fi
 
