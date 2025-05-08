d2590d29ff7fec7d1dca5c6d41c057d22046e9b7
Elyessto <66465116+Elyessto@users.noreply.github.com>
2025-05-08 10:26:37 +0100

    Update copy-sw.js

--- ui/scripts/copy-sw.js
+++ ui/scripts/copy-sw.js
@@ -1,7 +1,11 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-const rootPath = path.join(import.meta.dirname, '../');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//
+const rootPath = path.resolve(__dirname, '../');
 
 fs.cpSync(path.join(rootPath, 'dist-sw'), path.join(rootPath, 'dist'), { recursive: true });
 
